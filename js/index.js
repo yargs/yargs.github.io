d92c33f525fd22c8b580c84fd673c54e6b560afd
Iryna Shestak <shestak.irina@gmail.com>
2016-01-27 22:42:17 -0800

    remove jekyll's pre configured code styles; implement highlight.js

--- js/index.js
+++ js/index.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   var app = new App()
 
   app.angleDown.on("click", app.scrollToOverview)
+
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  })
 })
